c66efa09653c8eb9e3670f0471475abff75b12b2
dominic <dominicseduraja3@gmail.com>
2024-06-09 12:00:57 +0530

    finish side

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,18 @@ $(function() {
       items: 3,
       margin: 10,
       loop: true,
-      nav: true
+      // nav: true,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:2
+        },
+        1000:{
+          items:3
+        }
+      }
     });
   });
 
